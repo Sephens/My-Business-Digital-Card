82de81e78201477cebaa3b4be1eee48b0f83b2fa
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2023-01-25 23:43:09 +0300

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <div className='about'>
     <h2 className='about-head'>About</h2>
-    <p id="about-paragraph">I am a Fullstack developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+    <p id="about-paragraph">I am a Fullstack Web developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
     </div>
   )
 }
