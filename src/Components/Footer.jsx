6f9b324d9ca2983b737b394e0d74589aafe50e67
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2023-07-24 09:18:22 +0300

    update to add instagram link

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     <div className='footer'>
       <a href='https://twitter.com/_Sephens'><TiSocialTwitter className='social-icons' id='twitter'/></a>
       <a href='https://www.facebook.com/adenux.sephens'><TiSocialFacebook className='social-icons' id='facebook' /></a>
-      <a><TiSocialInstagram className='social-icons' id='instagram' href=''/></a>
+      <a href='https://www.instagram.com/_adenux/' ><TiSocialInstagram className='social-icons' id='instagram' href=''/></a>
       <a href='https://github.com/Sephens'><TiSocialGithubCircular className='social-icons' id='github' /></a>
     </div>
   )
